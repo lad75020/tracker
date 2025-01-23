f4ad7cccffa16235ec7ecef65f925687085a3e66
lad75020 <laurent@dubertrand.fr>
2025-01-23 14:12:04 +0100

    removed password from tornMoney.php

--- tornMoney.php
+++ tornMoney.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-$atlasURI = "mongodb+srv://laurent:11Atlas00!!@cluster0.ewchd67.mongodb.net/";
+
 $collection = (new MongoDB\Client())->TORN->Money;
 
 $jsonLogs = json_decode(file_get_contents("https://tracker.dubertrand.fr/Money.json" , false));
